481e7455f7122f8313d187ae9730c4e37ac13ecd
Sebastián Ramírez Magrí <sebasmagri@gmail.com>
2011-03-23 05:03:57 -0430

    No cambiar el nombre del proceso en NT por el momento dado que no existe libc. Queda pendiente encontrar una manera de cambiar el nombre del proceso en esa plataforma

--- turpial/main.py
+++ turpial/main.py
@@ -19,12 +19,13 @@ from turpial.api.turpialapi import TurpialAPI
 from turpial.config import ConfigHandler, ConfigApp, ConfigProtocol, PROTOCOLS
 from turpial.ui.cmd.main import Main as _CMD
 
-try:
-    import ctypes
-    libc = ctypes.CDLL('libc.so.6')
-    libc.prctl(15, 'turpial', 0, 0)
-except ImportError:
-    pass
+if os.name == 'posix':
+    try:
+        import ctypes
+        libc = ctypes.CDLL('libc.so.6')
+        libc.prctl(15, 'turpial', 0, 0)
+    except ImportError:
+        pass
 
 INTERFACES = ['cmd']
 try:
